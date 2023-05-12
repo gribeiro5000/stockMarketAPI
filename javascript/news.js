e7fb2906111e5e6f307a27c1075793c803ab75ac
Gabriel Ribeiro <gribeiro.gr17@gmail.com>
2023-05-11 21:13:04 -0300

    first commit

---
+++ javascript/news.js
@@ -0,0 +1,31 @@
+var apiKey = `1D0KB1CVSUC9D3T3`
+var urlNews = `https://www.alphavantage.co/query?function=NEWS_SENTIMENT&apikey=${apiKey}`
+
+var allNews = getAllNews()
+
+//get all news
+function getAllNews() {
+    fetch(urlNews).then(response => {
+        return response.json()
+    }).then(data => {
+        console.log(data)
+        createAllNewsPage(data)
+    })
+}
+
+function createAllNewsPage(allNews){
+    for(var pos = 0; pos < allNews.feed.length; pos++){
+        $(`#news`).append(`
+        <div class="card">
+            <a href="${allNews.feed[pos].url}">
+                <div class="title">   
+                    <h2>${allNews.feed[pos].title}</h2>
+                    <p>${allNews.feed[pos].summary}</p>
+                </div>
+                <img src="${allNews.feed[pos].banner_image}" alt="">
+            </a>
+        </div>
+        <hr size="1" width="100%">`);
+    }
+}
+
